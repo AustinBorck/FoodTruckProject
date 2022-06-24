8e6eb39cd621946c366dce122f13019ff5c6e091
Austin Borck <austinborck98@gmail.com>
2022-06-24 16:06:25 -0600

    test commit push

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
+public class FoodTruckApp {
+//ONLY STATIC METHOD
+//MENUS AND SCANNERS
+	
+	private FoodTruck [] fleetOfFoodTrucks;
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
